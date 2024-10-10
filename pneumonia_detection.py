707dcd4b4804aaf27e5ceb676cea490aa18d91d6
mininokire <70376924+mininokire@users.noreply.github.com>
2024-10-11 00:26:54 +0900

    Create pneumonia_detection.py
    医疗影像分析：肺炎X光图像分类

---
+++ pneumonia_detection.py
@@ -0,0 +1,52 @@
+# pneumonia_detection.py
+# 医疗影像分析：肺炎X光图像分类
+import torch
+import torchvision.transforms as transforms
+from torchvision import models
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import torch.nn as nn
+import torch.optim as optim
+
+# 图像数据预处理
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# 加载数据
+train_data = ImageFolder('data/train', transform=transform)
+test_data = ImageFolder('data/test', transform=transform)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+# 使用预训练模型
+model = models.resnet18(pretrained=True)
+model.fc = nn.Linear(model.fc.in_features, 2)  # 修改最后一层以适应2类分类问题
+
+# 训练设置
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# 训练模型
+def train_model(num_epochs=5):
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader)}")
+
+# 开始训练
+train_model()
+
+# 保存模型
+torch.save(model.state_dict(), 'pneumonia_model.pth')
